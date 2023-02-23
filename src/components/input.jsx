b4401b4923323248cfd8c92f17c0a290ebdb739f
c0s0cln <christopher.santelices0@walmart.com>
2023-02-23 16:51:27 -0300

    imagen touchable

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,48 +1,52 @@
-import { View, StyleSheet ,TextInput,Image} from 'react-native'
+import { View, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const input = ({style}) => {
-    
+const input = ({ style }) => {
 
-const [text, onChangeText] = React.useState(null);
 
-  return (
-    <View style={{...styles.container,...style}}>
-       <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder={'agrege una tarea'}
-      />
-     <Image
-        style={styles.add}
-        source={require('../images/add2.png')}
-      />
-    </View>
-  )
+    const [text, onChangeText] = React.useState(null);
+
+    return (
+        <View style={{ ...styles.container, ...style }}>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder={'agrege una tarea'}
+            />
+
+            <TouchableOpacity style={styles.button}>
+                <Image
+                    style={styles.add}
+                    source={require('../images/add2.png')}
+                />
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        flexDirection:"row"
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        alignSelf:"center"
     },
     input: {
-      height: 40,
-      width: "80%",
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-      borderColor:"white",
-      borderRadius:23, 
-      backgroundColor:"#7b7cef",
-      color:"white"
+        height: 40,
+        width: "80%",
+        margin: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: "white",
+        borderRadius: 15,
+        backgroundColor: "#7b7cef",
+        color: "white"
     },
-    add:{
-        marginTop:15,
+    add: {
+        marginTop: 16,
         width: 30,
         height: 30,
-        color:"white"
+        color: "white"
     }
 })
 
